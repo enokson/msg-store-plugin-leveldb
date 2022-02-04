daa0dd72f56de937afedf4d5c16e3c4481a4ea9f
joshua enokson <kilograhm@pm.me>
2022-02-04 14:49:37 -0800

    updated leveldb plugin, added benchmarks

---
+++ benches/general.rs
@@ -0,0 +1,66 @@
+extern crate bencher;
+use msg_store::Uuid;
+use msg_store_db_plugin::{Db, Bytes};
+use msg_store_plugin_leveldb::Leveldb;
+use std::{
+    fs::{remove_dir_all,create_dir_all},
+    path::Path,
+    time::Instant
+};
+
+const TMP_PATH: &'static str = "/tmp/msg-store-plugin-leveldb";
+
+fn dir_setup() {
+    let tmp_path = Path::new(TMP_PATH);
+    if tmp_path.exists() {
+        remove_dir_all(tmp_path).unwrap();
+    }
+    create_dir_all(tmp_path).unwrap();
+}
+
+fn dir_teardown() {
+    let tmp_path = Path::new(TMP_PATH);
+    if tmp_path.exists() {
+        remove_dir_all(tmp_path).unwrap();
+    }
+}
+
+fn writes() {
+    dir_setup();
+    let mut level = Leveldb::new(Path::new(TMP_PATH));
+    let msgs = (0..100_000).map(|i| {
+        let msg = Bytes::copy_from_slice("hello, world!".as_bytes());
+        let ln = msg.len() as u32;
+        (Uuid::from_string(&format!("1-1-{}", i)).unwrap(), msg, ln)
+    }).collect::<Vec<(Uuid, Bytes, u32)>>();
+    let start = Instant::now();
+    let _list = msgs.into_iter().map(|(uuid, msg, ln)| {
+        level.add(uuid, msg.clone(), ln).unwrap();
+        ()
+    }).collect::<Vec<()>>();
+    let end = start.elapsed().as_secs_f64();
+    println!("Total duration: {}, writes/sec: {}", end, (100_000.0)/end);
+    dir_teardown();
+}
+
+// fn a(bench: &mut Bencher) {
+//     bench.iter(|| {
+//         (0..1000).fold(0, |x, y| x + y)
+//     })
+// }
+
+// fn b(bench: &mut Bencher) {
+//     const N: usize = 1024;
+//     bench.iter(|| {
+//         vec![0u8; N]
+//     });
+ 
+//     bench.bytes = N as u64;
+// }
+
+fn main() {
+    writes();
+}
+
+// benchmark_group!(benches, writes);
+// benchmark_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 use bincode::{serialize, deserialize};
-use msg_store::{
-    Keeper,
-    store::{
-        Package,
-        PacketMetaData,
-        Store
-    },
-    uuid::Uuid
-};
+use msg_store::uuid::Uuid;
+use msg_store_db_plugin::{Bytes, Db};
 use db_key::Key;
 use leveldb::{
     database::Database,
@@ -20,54 +13,20 @@ use leveldb::{
     }
 };
 use serde::{Serialize, Deserialize};
-use std::{
-    fs::create_dir_all,
-    path::Path
-};
+use std::path::Path;
 
-pub type LevelStore = Store<Leveldb>;
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Serialize, Deserialize)]
-pub struct Id {
-    pub timestamp: u128,
-    pub sequence: u32
-}
-impl Id {
-    pub fn to_string(&self) -> String {
-        format!("{}-{}", self.timestamp, self.sequence)
-    }
-    pub fn from_string(id: &str) -> Uuid {
-        let split_str = id.split("-").collect::<Vec<&str>>();
-        Uuid { 
-            timestamp: split_str[0].parse().expect("Could not parse timestamp"), 
-            sequence: split_str[0].parse().expect("Could not parse sequence")
-        }
-    }
-    pub fn from_uuid(uuid: Uuid) -> Self {
-        Self {
-            timestamp: uuid.timestamp,
-            sequence: uuid.sequence
-        }
-    }
-    pub fn to_uuid(self) -> Uuid {
-        Uuid { 
-            timestamp: self.timestamp, 
-            sequence: self.sequence
-        }
-    }
-}
+#[derive(Deserialize, Serialize)]
+pub struct Id(Vec<u8>);
 
 impl Key for Id {
     fn from_u8(key: &[u8]) -> Self {
+        // Id(key.to_vec())
         deserialize(key).expect("Could not deserialize key")
     }
     fn as_slice<T, F: Fn(&[u8]) -> T>(&self, f: F) -> T {
+        // f(&self.0.to_vec())
         f(&serialize(&self).expect("Could not serialize uuid"))
     }
-}
-
-pub fn open(location: &Path) -> LevelStore {
-    Store::open(Leveldb::new(location))
 }
 
 pub struct Leveldb {
@@ -77,7 +36,6 @@ pub struct Leveldb {
 
 impl Leveldb {
     pub fn new(dir: &Path) -> Leveldb {
-        create_dir_all(&dir).expect("Could not create db location dir.");
 
         let mut msgs_path = dir.to_path_buf();
         msgs_path.push("msgs");
@@ -103,48 +61,122 @@ impl Leveldb {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct DbMetadata {
-    priority: i32,
-    byte_size: i32
-}
-
-impl Keeper for Leveldb {
-    fn add(&mut self, package: &Package) {
-        let data = DbMetadata {
-            priority: package.priority,
-            byte_size: package.byte_size
-        };
-        let serialized_data = serialize(&data).expect("Could not serialize data");
-        let msg = serialize(&package.msg).expect("Could not serialize msg");
-        self.data.put(WriteOptions::new(), Id::from_uuid(package.uuid), &serialized_data).expect("Could not insert metadata");
-        self.msgs.put(WriteOptions::new(), Id::from_uuid(package.uuid), &msg).expect("Could not insert msg");     
+impl Db for Leveldb {
+    fn add(&mut self, uuid: Uuid, msg: Bytes, msg_byte_size: u32) -> Result<(), String> {
+        let uuid_bytes = uuid.to_string().as_bytes().to_vec();
+        self.data.put(WriteOptions::new(), Id(uuid_bytes.clone()), format!("{}", msg_byte_size).as_bytes()).expect("Could not insert metadata");
+        self.msgs.put(WriteOptions::new(), Id(uuid_bytes), &msg).expect("Could not insert msg");
+        Ok(())
     }
-    fn get(&mut self, uuid: &Uuid) -> Option<String> {
-        match self.msgs.get(ReadOptions::new(), Id::from_uuid(*uuid)).expect("Could not get msg") {
-            Some(serialized_msg) => Some(deserialize(&serialized_msg).expect("Could not deserialize msg")),
-            None => None
-        }        
+    fn get(&mut self, uuid: Uuid) -> Result<Bytes, String> {
+        let uuid_bytes = uuid.to_string().as_bytes().to_vec();
+        match self.msgs.get(ReadOptions::new(), Id(uuid_bytes)).expect("Could not get msg") {
+            Some(msg) => Ok(Bytes::copy_from_slice(&msg)),
+            None => Err("Message not found".to_string())
+        }
     }
-    fn del(&mut self, uuid: &Uuid) {
-        self.msgs.delete(WriteOptions::new(), Id::from_uuid(*uuid)).expect("Could not delete msg");
+    fn del(&mut self, uuid: Uuid) -> Result<(), String> {
+        let uuid_bytes = uuid.to_string().as_bytes().to_vec();
+        self.msgs.delete(WriteOptions::new(), Id(uuid_bytes)).expect("Could not delete msg");
+        Ok(())
     }
-    fn fetch(&mut self) -> Vec<PacketMetaData> {
+    fn fetch(&mut self) -> Result<Vec<(Uuid, u32)>, String> {
         self.data.iter(ReadOptions::new()).map(|(id, data)| {
-            let db_metadata: DbMetadata = deserialize(&data).expect("Could not deserialize data");
-            PacketMetaData { 
-                uuid: id.to_uuid(),
-                priority: db_metadata.priority, 
-                byte_size: db_metadata.byte_size
-            }
-        }).collect::<Vec<PacketMetaData>>()
+            let data = match String::from_utf8(data) {
+                Ok(data) => data,
+                Err(_error) => {
+                    return Err("Could not convert bytes into String".to_string());
+                }
+            };
+            let data = match data.parse::<u32>() {
+                Ok(data) => data,
+                Err(_error) => {
+                    return Err("Could not parse u32".to_string());
+                }
+            };
+            let uuid = match String::from_utf8(id.0) {
+                Ok(uuid) => uuid,
+                Err(_error) => {
+                    return Err("Could not convert bytes into Uuid String".to_string())
+                }
+            };
+            let uuid = match Uuid::from_string(&uuid) {
+                Ok(uuid) => uuid,
+                Err(error) => {
+                    return Err(error)
+                }
+            };
+            Ok((uuid, data))
+        }).collect::<Result<Vec<(Uuid, u32)>, String>>()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use msg_store::Uuid;
+    use msg_store_db_plugin::{Db, Bytes};
+
+    use crate::{ Leveldb };
+    use std::{
+        fs::{
+            create_dir_all, remove_dir_all
+        },
+        path::{Path, PathBuf}, str::FromStr
+    };
+
+    fn dir_setup(tmp_dir: &Path) {
+        if tmp_dir.exists() {
+            remove_dir_all(&tmp_dir).unwrap();
+        }
+        create_dir_all(&tmp_dir).unwrap();
+    }
+
+    fn dir_teardown(tmp_dir: &Path) {
+        if tmp_dir.exists() {
+            remove_dir_all(&tmp_dir).unwrap();
+        }
+    }
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        // app  setup
+        let tmp_dir = PathBuf::from_str(&format!("/tmp/msg-store-plugin-leveldb")).unwrap();
+        dir_setup(&tmp_dir);
+
+        // create a fake uuid and message
+        let uuid = Uuid::from_string("1-0-1").unwrap();
+        let inner_msg = b"my message";
+        let msg = Bytes::copy_from_slice(inner_msg);
+        let msg_byte_size = inner_msg.len() as u32;
+        {
+            // get level instance
+            // add one message
+            // force out of scope
+            let mut level = Leveldb::new(&tmp_dir);
+            level.add(uuid, msg.clone(), msg_byte_size).unwrap();
+        }
+        // get level instance
+        let mut level = Leveldb::new(&tmp_dir);
+        
+        // fetch messages
+        let msgs = level.fetch().unwrap();
+        assert_eq!(1, msgs.len());
+        let (received_uuid, received_msg_byte_size) = msgs[0];
+        assert_eq!(uuid, received_uuid);
+        assert_eq!(msg_byte_size, received_msg_byte_size);
+
+        // get msg
+        let received_msg = level.get(uuid).unwrap();
+        assert_eq!(msg, received_msg);
+
+        // delete msg
+        level.del(uuid).unwrap();
+
+        let get_msg_result = level.get(uuid);
+        assert!(get_msg_result.is_err());
+
+        
+        // assert_eq!(2 + 2, 4);
+        dir_teardown(&tmp_dir);
     }
 }
